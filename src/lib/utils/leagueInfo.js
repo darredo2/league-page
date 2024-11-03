09277a0d607d7e48d1a0636ff1b4b1aca8b8cbcc
darredo2 <68441520+darredo2@users.noreply.github.com>
2024-11-03 09:28:16 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "1049238422889684992"; // your league ID
-export const leagueName = "League of Leagues Dynasty"; // your league name
+export const leagueID = "1124853991407104002"; // your league ID
+export const leagueName = "Super League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
